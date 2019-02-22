c894a9f2efe5a6e6b557785a3e6fac3c2f7a122c
HMW <howardmwiener@gmail.com>
2019-02-22 12:09:32 -0500

    added newMsg and createMg events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newEmail', {
+    from: 'person@wherever.com',
+    text: 'Whazzup?',
+    CreatedAt: 12345
+  });
+
+  socket.emit('newMsg', {
+      from: 'Msg user',
+      text: 'Message for you sir . . . ',
+      createdAt: 9876
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
+  socket.on('createMsg', (msg) => {
+    console.log('User sent msg: ', msg);
+  });
+
   socket.on('disconnect', (socket) => {
     console.log('User disconnected from server');
   });
